d08a457447d0dfc902c2e44664eec1e565ad38a2
Y.Y <c0a21131dd@edu.teu.ac.jp>
2022-11-22 15:30:28 +0900

    プロトタイプ

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,3 +1,17 @@
 if __name__ == "__main__":
     import random
-    import datetime
+
+    def shutudai(a):
+        QS = ["サザエの旦那の名前は？","カツオの妹の名前は？","タラオはカツオから見てどんな関係？"]
+        return QS[a]
+    def kaitou(a,ans):
+        ANS = [["マスオ","ますお"],["ワカメ","わかめ"],["甥","おい","甥っ子","おいっこ"]]
+        if ans in ANS[a]:
+            return "正解！！！"
+        else:
+            return "出直してこい"
+
+    a = random.randint(0,2)
+    print(f"問題：\n{shutudai(a)}")
+    ans = input("答えるんだ：")
+    print(kaitou(a,ans))
