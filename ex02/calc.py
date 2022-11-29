2a9b50f492a9dd6e8e7427ee2d6ed1ff865477d1
Y.Y <c0a21131dd@edu.teu.ac.jp>
2022-11-29 14:38:53 +0900

    練習

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,6 +5,7 @@ import tkinter.messagebox as tkm
 def button_click(event):
     btn = event.widget
     txt = btn["text"]
+    entry.insert(tk.END,f"{txt}")
     tkm.showinfo(txt,f"{txt}のボタンが押されました")
 
 
@@ -24,9 +25,19 @@ for num in range(9,-1,-1):
         k = 0
 
 
+operatas = ["+","="]
+for ope in operatas:
+    button = tk.Button(root,text=f"{ope}",font=("",30),width=4,height=2)
+    button.grid(row=j, column=k)
+    k += 1
+    if k % 3 == 0:
+        j += 1
+        k = 0
+
+
 entry = tk.Entry(root,justify="right",width=10,font=("",40))
-#entry.insert(tk.END)
 entry.grid(row=0,column=0, columnspan=3)
+
 
 
 root.mainloop()
