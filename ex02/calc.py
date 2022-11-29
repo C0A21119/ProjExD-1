8fb58e197ded716d03ad0b68f6edc455e7e69a96
Y.Y <c0a21131dd@edu.teu.ac.jp>
2022-11-29 14:03:58 +0900

    練習

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,6 +1,34 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    txt = btn["text"]
+    tkm.showinfo(txt,f"{txt}のボタンが押されました")
 
 root = tk.Tk()
 root.title("電卓")
 root.geometry("300x500")
+
+button9 = tk.Button(root,text="9",font=30,width=4,height=2)
+button8 = tk.Button(root,text="8",font=30,width=4,height=2)
+button7 = tk.Button(root,text="7",font=30,width=4,height=2)
+button6 = tk.Button(root,text="6",font=30,width=4,height=2)
+button5 = tk.Button(root,text="5",font=30,width=4,height=2)
+button4 = tk.Button(root,text="4",font=30,width=4,height=2)
+button3 = tk.Button(root,text="3",font=30,width=4,height=2)
+button2 = tk.Button(root,text="2",font=30,width=4,height=2)
+button1 = tk.Button(root,text="1",font=30,width=4,height=2)
+
+button9.grid(row=0, column=0)
+button8.grid(row=0, column=1)
+button7.grid(row=0, column=2)
+button6.grid(row=1, column=0)
+button5.grid(row=1, column=1)
+button4.grid(row=1, column=2)
+button3.grid(row=2, column=0)
+button2.grid(row=2, column=1)
+button1.grid(row=2, column=2)
+
+
 root.mainloop()
--- sample.py
+++ sample.py
@@ -1,6 +1,28 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    txt = btn["text"]
+    tkm.showwarning(txt,f"{txt}ボタンが押されました")
 
 root = tk.Tk()
 root.title("おためし")
 root.geometry("500x200")
+
+label = tk.Label(root,
+                text="ラベル",
+                font=("",20)
+                )
+label.pack()
+button = tk.Button(root,text="押すな")
+button.bind("<1>",button_click)
+button.pack()
+
+entry = tk.Entry(root,width=30)
+entry.insert(tk.END,"fugapiyo")
+entry.pack()
+
+
+
 root.mainloop()
