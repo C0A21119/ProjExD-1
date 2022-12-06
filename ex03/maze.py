1310284c88224bde2302092f83eb7a3d7df4ca14
Y.Y <c0a21131dd@edu.teu.ac.jp>
2022-12-06 14:11:49 +0900

    練習

--- ex03/maze.py
+++ ex03/maze.py
@@ -15,7 +15,9 @@ def key_up(event):
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
+    root.geometry("1500x900")
     canvas = tk.Canvas(root,width=1500,height=900,bg="black")
+    canvas.place(x=0, y=0)
     canvas.pack()
     image = tk.PhotoImage(file="fig/0.png")
     canvas.create_image(24,36.5,image=image)
