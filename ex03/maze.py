ac3d72d3f00ac289824a3a0d43976b5146c7f804
Y.Y <c0a21131dd@edu.teu.ac.jp>
2022-12-06 15:23:55 +0900

    基本完成

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,27 +2,35 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 import random
 import maze_maker as mm
+import time
 
 def key_down(event):
     global key
     key = event.keysym
-
 
 def key_up(event):
     global key
     key = ""
 
 def main_proc():
-    global cx, cy
+    global cx, cy, mx, my
     key_pressed = ["Up","Down","Right","Left"]
-    #move=[[0,-1],[0,1],[1,0],[-1,0]]
-    move=[[0,-20],[0,20],[20,0],[-20,0]]
+    move=[[0,-1],[0,1],[1,0],[-1,0]]
     for i,key_info in enumerate(key_pressed):
         if key == key_info:
-            cx += move[i][0]
-            cy += move[i][1]
+            if maze_list[mx+move[i][0]][my+move[i][1]]==0:
+                mx += move[i][0]
+                my += move[i][1]
+    cx, cy = mx*100+50, my*100+50
     canvas.coords("kokaton", cx, cy)
     root.after(100, main_proc)
+
+def time_time():
+    global now, tmr
+    tmr +=1
+    now = tmr
+    canvas.create_text(250, 0, text=now,anchor="ne", font=("",50))
+    root.after(1000, time_time())
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -35,10 +43,17 @@ if __name__ == "__main__":
     mm.show_maze(canvas, maze_list)
 
     tori = tk.PhotoImage(file=random.choice([f"fig/{i}.png" for i in range(10)]))
-    cx, cy = 300, 400
+    mx, my = 1, 1
+    cx, cy = mx*100+50, my*100+50
     canvas.create_image(cx,cy,image=tori, tag="kokaton")
+
+    now = "-"
+    tmr = 0
+    canvas.create_text(250, 0, text=now,anchor="ne", font=("",50))
+
     key = ""
     root.bind("<KeyPress>", key_down)
+    root.bind("<KeyPress>", time_time, "+")
     root.bind("<KeyRelease>", key_up)
     main_proc()
     root.mainloop()
